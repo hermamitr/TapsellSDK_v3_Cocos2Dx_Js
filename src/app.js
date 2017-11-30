c9388ee90bc9217908ac2a1de1adc340383ac0ce
AhmadrezaSaboor <ahmadreza.saboor2012@gmail.com>
2017-11-30 14:38:03 +0330

    add native video ad support, 26.0.2 build tools, gradle 4.1

--- src/app.js
+++ src/app.js
@@ -1,29 +1,82 @@
 var APP_KEY =
-	"rashssjnjiaeqqeihgjdsihajkbkqgeqqdoftpafmlcoofdflejgmttlercbsdfbnjnjqs";
-var ZONE_ID = "586e4ed1bc5c28712bd8d50c";
+	"qjmospqbfarbhodregqecbbnfhcjllkflpbpsmdrtpqkapdeptftldfiapfgbamkhalbij";
+var ZONE_ID = "59b4d07d468465281b792cb7";
+var NATIVE_BANNER_ZONEID = "59c8a9334684656c504f0e19";
+var NATIVE_VIDEO_ZONEID = "59c8ae514684656c504fce40";
 var AD_ID = "";
+var NATIVE_BANNER_AD_ID = "";
+var NATIVE_VIDEO_AD_ID = "";
+var nativeProps = {};
+var nativeMode = "banner";
 
 var HelloWorldLayer = cc.Layer.extend({
 	sprite: null,
 	ctor: function() {
 		this._super();
-
 		Tapsell.setDebugMode(true);
 		Tapsell.initialize(APP_KEY);
 		var size = cc.winSize;
 
 		var menuItem1 = new cc.MenuItemFont("RequestAd", onRequestAdClicked);
 		var menuItem2 = new cc.MenuItemFont("ShowAd", onShowAdClicked);
+		var menuItem3 = new cc.MenuItemFont(
+			"RequestNativeBannerAd",
+			onRequestNativeBannerAdClicked
+		);
+		var menuItem4 = new cc.MenuItemFont(
+			"RequestNativeVideoAd",
+			onRequestNativeVideoAdClicked
+		);
 
-		menuItem1.setPosition(cc.p(size.width / 2, size.height / 3 * 2));
-		menuItem2.setPosition(cc.p(size.width / 2, size.height / 3));
+		menuItem1.setPosition(cc.p(size.width / 2, size.height / 5 * 4));
+		menuItem2.setPosition(cc.p(size.width / 2, size.height / 5 * 3));
+		menuItem3.setPosition(cc.p(size.width / 2, size.height / 5 * 2));
+		menuItem4.setPosition(cc.p(size.width / 2, size.height / 5));
 
-		var menu = new cc.Menu(menuItem1, menuItem2);
+		var menu = new cc.Menu(menuItem1, menuItem2, menuItem3, menuItem4);
 		menu.setPosition(cc.p(0, 0));
-		this.addChild(menu, 0);
+		this.addChild(menu, 1);
 
-		Tapsell.setRewardListener((zoneId, adId, completed) => {
-			cc.log("Reward! " + completed);
+		var fontDef = new cc.FontDefinition();
+		fontDef.fontName = "Arial";
+		fontDef.fontSize = "24";
+		var myLabel = new cc.LabelTTF("NativeAd Title", fontDef);
+		myLabel.setPosition(cc.p(size.width / 2, size.height / 12));
+		nativeProps.title = myLabel;
+		var ctaListener = cc.EventListener.create({
+			event: cc.EventListener.TOUCH_ONE_BY_ONE,
+			swallowTouches: true,
+			onTouchBegan: function(touch, event) {
+				var target = event.getCurrentTarget();
+
+				var locationInNode = target.convertToNodeSpace(
+					touch.getLocation()
+				);
+				var s = target.getContentSize();
+				var rect = cc.rect(0, 0, s.width, s.height);
+
+				if (cc.rectContainsPoint(rect, locationInNode)) {
+					target.opacity = 180;
+					return true;
+				}
+				return false;
+			},
+			onTouchMoved: function(touch, event) {},
+			onTouchEnded: function(touch, event) {
+				var target = event.getCurrentTarget();
+				cc.log("CallToAction Clicked");
+				if (nativeMode == "banner")
+					Tapsell.onNativeBannerAdClicked(NATIVE_BANNER_AD_ID);
+				else if (nativeMode == "video")
+					Tapsell.onNativeVideoAdClicked(NATIVE_VIDEO_AD_ID);
+				target.opacity = 255;
+			}
+		});
+		cc.eventManager.addListener(ctaListener, myLabel);
+		this.addChild(myLabel, -1);
+
+		Tapsell.setRewardListener((zoneId, adId, completed, rewarded) => {
+			cc.log("Reward! " + completed + " " + rewarded);
 		});
 
 		return true;
@@ -78,6 +131,51 @@ var onShowAdClicked = function() {
 		},
 		function(adId) {
 			cc.log("onClosed");
+		}
+	);
+};
+var onRequestNativeBannerAdClicked = function() {
+	cc.log("RequestNativeBannerAd Clicked");
+	nativeMode = "banner";
+	Tapsell.requestNativeBannerAd(
+		NATIVE_BANNER_ZONEID,
+		function(adProps) {
+			cc.log("onAdAvailable : " + JSON.stringify(adProps));			
+			nativeProps.title.setString(adProps.description);
+			Tapsell.onNativeBannerAdShown(adProps.ad_id);
+			NATIVE_BANNER_AD_ID = adProps.ad_id;
+		},
+		function() {
+			cc.log("onNoAdAvailable");
+		},
+		function() {
+			cc.log("onNoNetwork");
+		},
+		function(error) {
+			cc.log("onError: " + error);
+		}
+	);
+};
+
+var onRequestNativeVideoAdClicked = function() {
+	cc.log("RequestNativeVideoAd Clicked");
+	nativeMode = "video";
+	Tapsell.requestNativeVideoAd(
+		NATIVE_VIDEO_ZONEID,
+		function(adProps) {
+			cc.log("onAdAvailable : " + JSON.stringify(adProps));
+			nativeProps.title.setString(adProps.description);
+			Tapsell.onNativeVideoAdShown(adProps.ad_id);
+			NATIVE_VIDEO_AD_ID = adProps.ad_id;
+		},
+		function() {
+			cc.log("onNoAdAvailable");
+		},
+		function() {
+			cc.log("onNoNetwork");
+		},
+		function(error) {
+			cc.log("onError: " + error);
 		}
 	);
 };
--- src/tapsell.js
+++ src/tapsell.js
@@ -1,7 +1,9 @@
 var requestAdCallbacks = {};
 var showAdCallbacks = {};
+var requestNativeBannerCallbacks = {};
+var requestNativeVideoCallbacks = {};
 var rewardListenerCb = undefined;
-var Tapsell, callbacks;
+var Tapsell, callbacks, nativeBannerCallbacks;
 
 if (cc.sys.os == cc.sys.OS_ANDROID) {
 	Tapsell = {
@@ -49,6 +51,84 @@ if (cc.sys.os == cc.sys.OS_ANDROID) {
 
 		setRewardListener: function(rewardListener) {
 			rewardListenerCb = rewardListener;
+		},
+
+		requestNativeBannerAd: function(
+			zoneId,
+			onAdAvailable,
+			onNoAdAvailable,
+			onNoNetwork,
+			onError
+		) {
+			requestNativeBannerCallbacks[zoneId] = {
+				ON_AD_AVAILABLE_CB: onAdAvailable,
+				ON_ERROR_CB: onError,
+				ON_NO_AD_AVAILABLE_CB: onNoAdAvailable,
+				ON_NO_NETWORK_CB: onNoNetwork
+			};
+			jsb.reflection.callStaticMethod(
+				"org/cocos2dx/javascript/Tapsell",
+				"requestNativeBannerAd",
+				"(Ljava/lang/String;)V",
+				zoneId
+			);
+		},
+
+		onNativeBannerAdShown: function(adId) {
+			jsb.reflection.callStaticMethod(
+				"org/cocos2dx/javascript/Tapsell",
+				"nativeBannerAdShown",
+				"(Ljava/lang/String;)V",
+				adId
+			);
+		},
+
+		onNativeBannerAdClicked: function(adId) {
+			jsb.reflection.callStaticMethod(
+				"org/cocos2dx/javascript/Tapsell",
+				"nativeBannerAdClicked",
+				"(Ljava/lang/String;)V",
+				adId
+			);
+		},
+
+		requestNativeVideoAd: function(
+			zoneId,
+			onAdAvailable,
+			onNoAdAvailable,
+			onNoNetwork,
+			onError
+		) {
+			requestNativeVideoCallbacks[zoneId] = {
+				ON_AD_AVAILABLE_CB: onAdAvailable,
+				ON_ERROR_CB: onError,
+				ON_NO_AD_AVAILABLE_CB: onNoAdAvailable,
+				ON_NO_NETWORK_CB: onNoNetwork
+			};
+			jsb.reflection.callStaticMethod(
+				"org/cocos2dx/javascript/Tapsell",
+				"requestNativeVideoAd",
+				"(Ljava/lang/String;)V",
+				zoneId
+			);
+		},
+
+		onNativeVideoAdShown: function(adId) {
+			jsb.reflection.callStaticMethod(
+				"org/cocos2dx/javascript/Tapsell",
+				"nativeVideoAdShown",
+				"(Ljava/lang/String;)V",
+				adId
+			);
+		},
+
+		onNativeVideoAdClicked: function(adId) {
+			jsb.reflection.callStaticMethod(
+				"org/cocos2dx/javascript/Tapsell",
+				"nativeVideoAdClicked",
+				"(Ljava/lang/String;)V",
+				adId
+			);
 		},
 
 		initialize: function(appKey) {
@@ -133,6 +213,74 @@ if (cc.sys.os == cc.sys.OS_ANDROID) {
 		ROTATION_UNLOCKED: 3,
 		ROTATION_LOCKED_REVERSED_LANDSCAPE: 4,
 		ROTATION_LOCKED_REVERSED_PORTRAIT: 5
+	};
+
+	nativeBannerCallbacks = {
+		onAdAvailable: function(zoneId, adId) {
+			if (requestNativeBannerCallbacks[zoneId]["ON_AD_AVAILABLE_CB"]) {
+				requestNativeBannerCallbacks[zoneId]["ON_AD_AVAILABLE_CB"](
+					adId
+				);
+				requestNativeBannerCallbacks[zoneId][
+					"ON_AD_AVAILABLE_CB"
+				] = undefined;
+			}
+		},
+		onNoAdAvailable: function(zoneId) {
+			if (requestNativeBannerCallbacks[zoneId]["ON_NO_AD_AVAILABLE_CB"]) {
+				requestNativeBannerCallbacks[zoneId]["ON_NO_AD_AVAILABLE_CB"]();
+				requestNativeBannerCallbacks[zoneId][
+					"ON_NO_AD_AVAILABLE_CB"
+				] = undefined;
+			}
+		},
+		onNoNetwork: function(zoneId) {
+			if (requestNativeBannerCallbacks[zoneId]["ON_NO_NETWORK_CB"]) {
+				requestNativeBannerCallbacks[zoneId]["ON_NO_NETWORK_CB"]();
+				requestNativeBannerCallbacks[zoneId][
+					"ON_NO_NETWORK_CB"
+				] = undefined;
+			}
+		},
+		onError: function(zoneId, error) {
+			if (requestNativeBannerCallbacks[zoneId]["ON_ERROR_CB"]) {
+				requestNativeBannerCallbacks[zoneId]["ON_ERROR_CB"](error);
+				requestNativeBannerCallbacks[zoneId]["ON_ERROR_CB"] = undefined;
+			}
+		}
+	};
+
+	nativeVideoCallbacks = {
+		onAdAvailable: function(zoneId, adId) {
+			if (requestNativeVideoCallbacks[zoneId]["ON_AD_AVAILABLE_CB"]) {
+				requestNativeVideoCallbacks[zoneId]["ON_AD_AVAILABLE_CB"](adId);
+				requestNativeVideoCallbacks[zoneId][
+					"ON_AD_AVAILABLE_CB"
+				] = undefined;
+			}
+		},
+		onNoAdAvailable: function(zoneId) {
+			if (requestNativeVideoCallbacks[zoneId]["ON_NO_AD_AVAILABLE_CB"]) {
+				requestNativeVideoCallbacks[zoneId]["ON_NO_AD_AVAILABLE_CB"]();
+				requestNativeVideoCallbacks[zoneId][
+					"ON_NO_AD_AVAILABLE_CB"
+				] = undefined;
+			}
+		},
+		onNoNetwork: function(zoneId) {
+			if (requestNativeVideoCallbacks[zoneId]["ON_NO_NETWORK_CB"]) {
+				requestNativeVideoCallbacks[zoneId]["ON_NO_NETWORK_CB"]();
+				requestNativeVideoCallbacks[zoneId][
+					"ON_NO_NETWORK_CB"
+				] = undefined;
+			}
+		},
+		onError: function(zoneId, error) {
+			if (requestNativeVideoCallbacks[zoneId]["ON_ERROR_CB"]) {
+				requestNativeVideoCallbacks[zoneId]["ON_ERROR_CB"](error);
+				requestNativeVideoCallbacks[zoneId]["ON_ERROR_CB"] = undefined;
+			}
+		}
 	};
 
 	callbacks = {
